574800052ce40195b719bff93157875e6dbe1f71
hbwxl <15652964580@163.com>
2016-11-28 11:12:33 +0800

    只有3个勇士了。。

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include "iostream"
 #include "string"
+#include "bloom.h"
 #include <malloc.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -15,26 +16,30 @@
 #include <event2/event.h>
 #include <event2/http.h>
 #include <event2/bufferevent.h>
-
-#define THREAD_MAX_NUM 50
+//#include <libltdl/lt_system.h>
+#include "thread_pool.h"
+int pagenumber=0;
+int thcount=0;
+#define THREAD_MAX_NUM 200
 FILE *f;
 using namespace std;
 
 
 pthread_mutex_t write_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t thread_num_lock = PTHREAD_MUTEX_INITIALIZER;
-int thread_num = 0;
 pthread_mutex_t queue_lock = PTHREAD_MUTEX_INITIALIZER;
 queue<string>url_queue;
-int thread_stat[THREAD_MAX_NUM] = {0};
+
+threadpool threadpool_spider;
+
+BLOOM* bloom_url;
 
 struct fuck {
-    int thread_num;
     char url[300];
 };//给线程传递参数
 
 void href_finder(char *source_page, char *parent_url)
 {
+    //int coutpage=0;
     char *pointer, *space;
     pointer = (char*)malloc(1040*sizeof(char));
     space = pointer;
@@ -55,13 +60,15 @@ void href_finder(char *source_page, char *parent_url)
             pointer = url_end + 2;
 
             strcat(new_url, url);
-            if(url[0] == '/' || url[0] == '?')
+            if(url[0] == '/' || strstr(url,"#"))//||strstr(url,".htm")
             {
                 free(new_url);
                 continue;
             } else
             {
+
                 pthread_mutex_lock(&queue_lock);
+                //bloom_add(bloom_url,new_url,sizeof(new_url));
                 string string_url(new_url);
                 url_queue.push(string_url);
                 pthread_mutex_unlock(&queue_lock);
@@ -70,6 +77,7 @@ void href_finder(char *source_page, char *parent_url)
         } else {
             break;
         }
+
     }
 
     free(space);
@@ -78,12 +86,12 @@ void href_finder(char *source_page, char *parent_url)
 void http_request_done(struct evhttp_request *req, void *arg){
     char buf[1024];
     int s;
-
+    //int countpage=0;
     char current_url[300] = {'\0'};
     if(evhttp_request_get_uri(req)){
         pthread_mutex_lock(&write_lock);
-        f = fopen("//home/wxl/文档/3000warriors/URL.txt", "a");
-        fprintf(f, "%s\n", evhttp_request_get_uri(req));
+        f = fopen("//home/wxl/文档/new/URL.txt", "a");
+        fprintf(f, "%d %s %d\n",pagenumber++, evhttp_request_get_uri(req),(int)(req->body_size));
         fclose(f);
         pthread_mutex_unlock(&write_lock);
     }
@@ -92,22 +100,21 @@ void http_request_done(struct evhttp_request *req, void *arg){
     while(s = evbuffer_remove(req->input_buffer, &buf, sizeof(buf) - 1))
     {
         buf[s] = '\0';
-        if(!strstr(evhttp_request_get_uri(req), "html"))
+       // printf("%d\n",(int)sizeof(buf));
+        if(!strstr(evhttp_request_get_uri(req), "html")&&!strstr(evhttp_request_get_uri(req), ".htm")&&!strstr(evhttp_request_get_uri(req), ".gif")&&!strstr(evhttp_request_get_uri(req),".js")&&!strstr(evhttp_request_get_uri(req), ".swf")&&!strstr(evhttp_request_get_uri(req), "?"))//||!strstr(evhttp_request_get_uri(req), ".js")||!strstr(evhttp_request_get_uri(req), ".css")||!strstr(evhttp_request_get_uri(req), ".htm")
         {
             href_finder(buf, current_url);
         }
 
     }
-    // terminate event_base_dispatch()
     event_base_loopbreak((struct event_base *)arg);
 }
 
-void *hello_world_thread(void *arg)
+void hello_world_thread(void *arg)
 {
 
 
     struct fuck *new_arg = (struct fuck*)arg;
-    printf("%d   ", new_arg->thread_num);
     printf("%s\n",new_arg->url);
 
     struct event_base *base;
@@ -123,79 +130,69 @@ void *hello_world_thread(void *arg)
     if(!req)
         printf("New Req Fail!!!!!!\n");
     evhttp_add_header(req->output_headers, "Host", "localhost");
-    char *req_url = (char *)calloc(200, sizeof(char));
+    char *req_url = (char *)calloc(256, sizeof(char));
     strcpy(req_url, new_arg->url);
     evhttp_make_request(conn, req, EVHTTP_REQ_GET, req_url);
     evhttp_connection_set_timeout(req->evcon, 600);
 
     event_base_dispatch(base);
-//    event_base_free(base);
-
-
-
 
     free(req_url);
-    //printf("%d\n", thread_num);
-    //evhttp_request_free(req);
     evhttp_connection_free(conn);
     event_base_free(base);
-    pthread_mutex_lock(&thread_num_lock);
-    thread_stat[new_arg->thread_num]=0;
-
-   // printf("%d   get out!\n",new_arg->thread_num);
-    //printf("set %d to 0\n",new_arg->thread_num);
-    pthread_mutex_unlock(&thread_num_lock);
 
     free(new_arg);
     pthread_detach(pthread_self());
-    return NULL;
+    thcount=0;
 
-}//
+}
 
 int main()
 {
-    int rc;
+    int count = 0;
     url_queue.push("/sohu/");
     pthread_t threads[THREAD_MAX_NUM];
-    char *s = (char *) calloc(128, sizeof(char));
+    char *s = (char *) calloc(256, sizeof(char));
+
+    if(!(bloom_url=bloom_create(4000000, 10)))
+    {
+        printf("Failure to create bloom filter.\n");
+        return EXIT_FAILURE;
+    }else{
+        printf("Success to create bloom filter.\n");
+    }
+
+    threadpool_spider = create_threadpool(THREAD_MAX_NUM);
 
     while (1)
     {
-        for (int i = 0; i < THREAD_MAX_NUM; i++) {
-            if (thread_stat[i] == 0) {
-                if (!url_queue.empty()) {
+        if (!url_queue.empty())
+        {
+            //count = 0;
+            pthread_mutex_lock(&queue_lock);
+            strcpy(s, url_queue.front().c_str());
+            url_queue.pop();
+            pthread_mutex_unlock(&queue_lock);
+            //thcount=0;
+            struct fuck *new_fuck = (struct fuck*)malloc(sizeof(struct fuck));
+            strcpy(new_fuck->url, s);
+            dispatch(threadpool_spider, hello_world_thread, new_fuck);
 
-                    pthread_mutex_lock(&queue_lock);
-                   // printf("pop %s\n",url_queue.front().c_str());
-                    strcpy(s, url_queue.front().c_str());
-                    url_queue.pop();
-                    pthread_mutex_unlock(&queue_lock);
-
-                    //pthread_mutex_lock(&thread_num_lock);
-                    thread_stat[i]=1;
-                    struct fuck *new_fuck = (struct fuck*)malloc(sizeof(struct fuck));
-                    new_fuck->thread_num = i;
-                    strcpy(new_fuck->url, s);
-                    rc = pthread_create(&threads[i], NULL, hello_world_thread, new_fuck);
-                    //pthread_mutex_unlock(&thread_num_lock);
-                    if (rc)
-                    {
-                        printf("ERROR: pthread_create failed with %d\n", rc);
-                        return -1;
-                    }
-                } else sleep(3);
-            }
         }
+        else{
+            sleep(1);
+            thcount++;
+            if(thcount == 30){
+                break;
+            }
+
+        }
+
 
     }
     free(s);
     printf("INFO: All threads have been exit!!!\n");
-    while (!url_queue.empty()) {
-        pthread_mutex_lock(&queue_lock);
-        cout << url_queue.front() << endl;
-        url_queue.pop();
-        pthread_mutex_unlock(&queue_lock);
-    }
+
 
     return 0;
 }
