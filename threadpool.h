66b49ec1ee44ffd91b40e5db2d38a3e49c63218c
hbwxl <15652964580@163.com>
2016-11-28 11:13:23 +0800

    替换成锦涛的

--- threadpool.h
+++ threadpool.h
@@ -1,116 +1,81 @@
-/*
- * Copyright (c) 2013, Mathias Brossard <mathias@brossard.org>.
- * All rights reserved.
+//
+// Created by haojintao on 16-11-27.
+//
+
+#ifndef INC_3000WARRIORS_THREAD_POOL_H
+#define INC_3000WARRIORS_THREAD_POOL_H
+
+/**
+ * threadpool.h
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
+ * This file declares the functionality associated with
+ * your implementation of a threadpool.
+ */
+
+// maximum number of threads allowed in a pool
+#define MAXT_IN_POOL 200
+
+// You must hide the internal details of the threadpool
+// structure from callers, thus declare threadpool of type "void".
+// In threadpool.c, you will use type conversion to coerce
+// variables of type "threadpool" back and forth to a
+// richer, internal type.  (See threadpool.c for details.)
+
+typedef void *threadpool;
+
+// "dispatch_fn" declares a typed function pointer.  A
+// variable of type "dispatch_fn" points to a function
+// with the following signature:
+//
+//     void dispatch_function(void *arg);
+
+typedef void (*dispatch_fn)(void *);
+
+/**
+ * create_threadpool creates a fixed-sized thread
+ * pool.  If the function succeeds, it returns a (non-NULL)
+ * "threadpool", else it returns NULL.
+ */
+threadpool create_threadpool(int num_threads_in_pool);
+
+/**
+ * dispatch sends a thread off to do some work.  If
+ * all threads in the pool are busy, dispatch will
+ * block until a thread becomes free and is dispatched.
  *
- *  1. Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
+ * Once a thread is dispatched, this function returns
+ * immediately.
  *
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * The dispatched thread calls into the function
+ * "dispatch_to_here" with argument "arg".
  */
-
-#ifndef _THREADPOOL_H_
-#define _THREADPOOL_H_
-
-#ifdef __cplusplus
-/* 对于 C++ 编译器，指定用 C 的语法编译 */
-extern "C" {
-#endif
+void dispatch(threadpool from_me, dispatch_fn dispatch_to_here,
+              void *arg);
 
 /**
- * @file threadpool.h
- * @brief Threadpool Header File
+ * Works the same as dispatch, but enables the user to define cleanup handlers in
+ * cases of immediate cancel.  The cleanup handler function (cleaner_func) is
+ * executed automatically with cleaner_arg as the argument after the
+ * dispatch_to_here function is done; in the case that destroy_threadpool_immediately
+ * is called, cleaner_func is also called before the thread exits.
  */
-
- /**
- * Increase this constants at your own risk
- * Large values might slow down your system
- */
-#define MAX_THREADS 64
-#define MAX_QUEUE 65536
-
-/* 简化变量定义 */
-typedef struct threadpool_t threadpool_t;
-
-/* 定义错误码 */
-typedef enum {
-    threadpool_invalid        = -1,
-    threadpool_lock_failure   = -2,
-    threadpool_queue_full     = -3,
-    threadpool_shutdown       = -4,
-    threadpool_thread_failure = -5
-} threadpool_error_t;
-
-typedef enum {
-    threadpool_graceful       = 1
-} threadpool_destroy_flags_t;
-
-/* 以下是线程池三个对外 API */
+void dispatch_with_cleanup(threadpool from_me, dispatch_fn dispatch_to_here,
+                           void * arg, dispatch_fn cleaner_func, void* cleaner_arg);
 
 /**
- * @function threadpool_create
- * @brief Creates a threadpool_t object.
- * @param thread_count Number of worker threads.
- * @param queue_size   Size of the queue.
- * @param flags        Unused parameter.
- * @return a newly created thread pool or NULL
+ * destroy_threadpool kills the threadpool, causing
+ * all threads in it to commit suicide, and then
+ * frees all the memory associated with the threadpool.
  */
-/**
- * 创建线程池，有 thread_count 个线程，容纳 queue_size 个的任务队列，flags 参数没有使用
- */
-threadpool_t *threadpool_create(int thread_count, int queue_size, int flags);
+void destroy_threadpool(threadpool destroyme);
 
 /**
- * @function threadpool_add
- * @brief add a new task in the queue of a thread pool
- * @param pool     Thread pool to which add the task.
- * @param function Pointer to the function that will perform the task.
- * @param argument Argument to be passed to the function.
- * @param flags    Unused parameter.
- * @return 0 if all goes well, negative values in case of error (@see
- * threadpool_error_t for codes).
+ * destroy_threadpool_immediately cancels all threads in the threadpool immediately.
+ * It is potentially dangerous to use with libraries that are not specifically
+ * asynchronous cancel thread safe.  Also, without cleanup handlers, any dynamic
+ * memory or system resource in use could potentially be left without a reference
+ * to it.
  */
-/**
- *  添加任务到线程池, pool 为线程池指针，routine 为函数指针， arg 为函数参数， flags 未使用
- */
-int threadpool_add(threadpool_t *pool, void (*routine)(void *),
-                   void *arg, int flags);
+void destroy_threadpool_immediately(threadpool destroymenow);
 
-/**
- * @function threadpool_destroy
- * @brief Stops and destroys a thread pool.
- * @param pool  Thread pool to destroy.
- * @param flags Flags for shutdown
- *
- * Known values for flags are 0 (default) and threadpool_graceful in
- * which case the thread pool doesn't accept any new tasks but
- * processes all pending tasks before shutdown.
- */
-/**
- * 销毁线程池，flags 可以用来指定关闭的方式
- */
-int threadpool_destroy(threadpool_t *pool, int flags);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* _THREADPOOL_H_ */
-
+#endif //INC_3000WARRIORS_THREAD_POOL_H
