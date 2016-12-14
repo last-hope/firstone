75388cbf028ce91ad2db11d994124b23f03d6ce0
hbwxl <15652964580@163.com>
2016-12-14 15:42:58 +0800

    12.14 更改网址

--- main.cpp
+++ main.cpp
@@ -20,7 +20,8 @@
 #include "thread_pool.h"
 int pagenumber=0;
 int thcount=0;
-#define THREAD_MAX_NUM 200
+#define THREAD_MAX_NUM 1
+char new_url1[16]="/news.sohu.com/";
 FILE *f;
 using namespace std;
 
@@ -28,6 +29,7 @@ using namespace std;
 pthread_mutex_t write_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t queue_lock = PTHREAD_MUTEX_INITIALIZER;
 queue<string>url_queue;
+char filename[50];
 
 threadpool threadpool_spider;
 
@@ -46,35 +48,76 @@ void href_finder(char *source_page, char *parent_url)
     strcpy(pointer, source_page);
     char *url_start, *url_end;
     char url[300];
+    //char new_url[300];
+    int a=0;
 
-    while(url_start = strstr(pointer, "href=\""))
+    while((url_start = strstr(pointer, "href=\""))!=NULL)
     {
         url_start += 6;
-        url_end = strstr(url_start, "\"");
-        if (url_end) {
-            char *new_url = (char *) calloc(300, sizeof(char));
-            strcpy(new_url, parent_url);
-            strncpy(url, url_start, url_end-url_start);
+        if(url_start=='\0'){
+            break;
+        }
 
+
+        url_end = strstr(url_start, "\"");
+        if (url_end!=NULL) {
+            char *new_url = (char *) calloc(300, sizeof(char));
+
+            //strcpy(new_url, parent_url);
+            if(url_end-url_start>=300){
+                //printf("%s dayu300",)
+                break;
+            }
+
+            strncpy(url, url_start, url_end-url_start);
             url[url_end-url_start] = '\0';
             pointer = url_end + 2;
 
-            strcat(new_url, url);
-            if(url[0] == '/' || strstr(url,"#"))//||strstr(url,".htm")
+            if(strstr(url,"#")|| strstr(url,"?")|| strstr(url,"http")||strstr(url," ")||strstr(url,"java")||strstr(url,".com"))
             {
-                free(new_url);
+                //free(url);
                 continue;
-            } else
-            {
-
-                pthread_mutex_lock(&queue_lock);
-                //bloom_add(bloom_url,new_url,sizeof(new_url));
-                string string_url(new_url);
-                url_queue.push(string_url);
-                pthread_mutex_unlock(&queue_lock);
             }
+            else if(url[0]=='/')
+            {
+                strcat(new_url, new_url1);
+                strcat(new_url, url+1);
+                //pthread_mutex_lock(&queue_lock);
+                a=bloom_check(bloom_url,url,sizeof(url)-1);
+             //   pthread_mutex_unlock(&queue_lock);
+                if (a==1){
+
+                    printf("%s youle\n",new_url);
+                    free(new_url);
+                    continue;
+                }
+            }
+            else if(url[0]!='/')
+            {
+                strcat(new_url, new_url1);
+                strcat(new_url, url);
+
+                //pthread_mutex_lock(&queue_lock);
+                a=bloom_check(bloom_url,url,sizeof(url)-1);
+               // pthread_mutex_unlock(&queue_lock);
+                if (a==1){
+                    printf("%s youle\n",new_url);
+                    free(new_url);
+                    continue;
+                }
+            }
+            printf("%s %d\n",parent_url,0);
+            printf("%s %d\n",url,1);
+            printf("%s %d\n",new_url,2);
+            pthread_mutex_lock(&queue_lock);
+            bloom_add(bloom_url,url,sizeof(url)-1);
+            string string_url(new_url);
+            url_queue.push(string_url);
+            pthread_mutex_unlock(&queue_lock);
+            free(new_url);
 
         } else {
+
             break;
         }
 
@@ -87,26 +130,40 @@ void http_request_done(struct evhttp_request *req, void *arg){
     char buf[1024];
     int s;
     //int countpage=0;
+   // char *html_content = (char *)calloc(sizeof(char) , 1024199 );
     char current_url[300] = {'\0'};
+    if(req==NULL){
+        event_base_loopbreak((struct event_base *)arg);
+        return;
+    }
     if(evhttp_request_get_uri(req)){
         pthread_mutex_lock(&write_lock);
-        f = fopen("//home/wxl/文档/new/URL.txt", "a");
+        f = fopen(filename, "a");
         fprintf(f, "%d %s %d\n",pagenumber++, evhttp_request_get_uri(req),(int)(req->body_size));
         fclose(f);
         pthread_mutex_unlock(&write_lock);
     }
     strcpy(current_url, evhttp_request_get_uri(req));
 
-    while(s = evbuffer_remove(req->input_buffer, &buf, sizeof(buf) - 1))
+    while((s = evbuffer_remove(req->input_buffer, &buf, sizeof(buf) - 1)))
     {
+        if(s==-1)
+            break;
         buf[s] = '\0';
        // printf("%d\n",(int)sizeof(buf));
-        if(!strstr(evhttp_request_get_uri(req), "html")&&!strstr(evhttp_request_get_uri(req), ".htm")&&!strstr(evhttp_request_get_uri(req), ".gif")&&!strstr(evhttp_request_get_uri(req),".js")&&!strstr(evhttp_request_get_uri(req), ".swf")&&!strstr(evhttp_request_get_uri(req), "?"))//||!strstr(evhttp_request_get_uri(req), ".js")||!strstr(evhttp_request_get_uri(req), ".css")||!strstr(evhttp_request_get_uri(req), ".htm")
+        if(!strstr(evhttp_request_get_uri(req), ".gif")&&!strstr(evhttp_request_get_uri(req),".js")&&!strstr(evhttp_request_get_uri(req), ".swf")&&!strstr(evhttp_request_get_uri(req),".css"))//||!strstr(evhttp_request_get_uri(req), ".js")||!strstr(evhttp_request_get_uri(req), ".css")||!strstr(evhttp_request_get_uri(req), ".htm")
         {
+            //strcat(html_content, buf);
             href_finder(buf, current_url);
         }
 
     }
+   /* if(!strstr(evhttp_request_get_uri(req), "html")&&!strstr(evhttp_request_get_uri(req), ".htm")&&!strstr(evhttp_request_get_uri(req), ".gif")&&!strstr(evhttp_request_get_uri(req),".js")&&!strstr(evhttp_request_get_uri(req), ".swf")&&!strstr(evhttp_request_get_uri(req),".css"))//||!strstr(evhttp_request_get_uri(req), ".js")||!strstr(evhttp_request_get_uri(req), ".css")||!strstr(evhttp_request_get_uri(req), ".htm")
+    {
+        href_finder(html_content, current_url);
+        free(html_content);
+        html_content = NULL;
+    }*/
     event_base_loopbreak((struct event_base *)arg);
 }
 
@@ -115,7 +172,7 @@ void hello_world_thread(void *arg)
 
 
     struct fuck *new_arg = (struct fuck*)arg;
-    printf("%s\n",new_arg->url);
+    printf("%s hello_world_thread\n",new_arg->url);
 
     struct event_base *base;
     struct evhttp_connection *conn;
@@ -123,7 +180,7 @@ void hello_world_thread(void *arg)
     base = event_base_new();
     if(!base)
         printf("New Event Base Fail!!!!!!\n");
-    conn = evhttp_connection_base_new(base, NULL, "10.108.87.34", 80);
+    conn = evhttp_connection_base_new(base, NULL, "10.108.84.118", 80);
     if(!conn)
         printf("New Conn Fail!!!!!!\n");
     req = evhttp_request_new(http_request_done, base);
@@ -147,14 +204,20 @@ void hello_world_thread(void *arg)
 
 }
 
-int main()
-{
-    int count = 0;
-    url_queue.push("/sohu/");
-    pthread_t threads[THREAD_MAX_NUM];
+int main(int argc, char * argv[]){
+
+   /* if(argc!=3){
+        printf("USAGE:crawel address outputfile\n");
+        return 0;
+    }*/
+    strcpy(filename, "new_output.txt");
+  //  int count = 0;
+    //url_queue.push(strchr(argv[1], '4')+5);
+    url_queue.push("/news.sohu.com/");
+  //  pthread_t threads[THREAD_MAX_NUM];
     char *s = (char *) calloc(256, sizeof(char));
 
-    if(!(bloom_url=bloom_create(4000000, 10)))
+    if(!(bloom_url=bloom_create(40000000, 17)))
     {
         printf("Failure to create bloom filter.\n");
         return EXIT_FAILURE;
@@ -163,15 +226,17 @@ int main()
     }
 
     threadpool_spider = create_threadpool(THREAD_MAX_NUM);
-
+    bloom_add(bloom_url,"/news.sohu.com/",16);
     while (1)
     {
         if (!url_queue.empty())
         {
             //count = 0;
+           // printf("ff\n");
             pthread_mutex_lock(&queue_lock);
             strcpy(s, url_queue.front().c_str());
             url_queue.pop();
+           // printf("gg\n");
             pthread_mutex_unlock(&queue_lock);
             //thcount=0;
             struct fuck *new_fuck = (struct fuck*)malloc(sizeof(struct fuck));
@@ -182,6 +247,7 @@ int main()
         else{
             sleep(1);
             thcount++;
+            printf("%d\n",thcount);
             if(thcount == 30){
                 break;
             }
